338856b105f509560a711f1f55ad7bdbd083bfd5
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-01-22 14:07:17 -0500

    Update 5-useState-counter.js

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const changeValue = (e) => {
+    console.log(e.target.textContent);
+    if (e.target.textContent === "decrease") {
+      setValue(value - 1);
+    } else if (e.target.textContent === "increase") {
+      setValue(value + 1);
+    } else {
+      setValue(0);
+    }
+  };
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1); if you click increase button 3 times it only increases once
+      // because each time it looks at old state value
+      setValue((prevState) => {
+        // here we get the previous state value and add 1
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button
+          className="btn"
+          onClick={(e) => {
+            changeValue(e);
+          }}
+        >
+          decrease
+        </button>
+        <button
+          className="btn"
+          onClick={(e) => {
+            changeValue(e);
+          }}
+        >
+          reset
+        </button>
+        <button
+          className="btn"
+          onClick={(e) => {
+            changeValue(e);
+          }}
+        >
+          increase
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          increase later
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
