6529e06890a9225b9dd7893b11d96e499adf6502
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-01-22 14:06:59 -0500

    Update 2-useState-basics.js

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+// useState - function
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState());
+  // const text = useState('random title')[0]
+  // const setText = useState('random title')[1]
+  const [text, setText] = useState("random title");
+
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("hello world");
+    } else {
+      setText("random title");
+    }
+  };
+
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
