018b035bba61fbfa3811205349610c8a8e68cf32
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-01-25 09:37:16 -0500

    Update 3-useEffect-fetch-data.js

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const res = await fetch(url);
+    const users = await res.json();
+    // setUsers triggers a rerender so you must set useEffect dependency list to [] or will get inifite loop or rerenders
+    setUsers(users);
+    // console.log(users);
+  };
+  // can't make useEffect an async/await b/c async returns a promise
+  // you can setup an async function w/in useEffect callback fn or set up separate fn
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
